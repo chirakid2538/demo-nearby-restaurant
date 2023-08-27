0985579ba60e0672c984ec901f17ed7788857cc1
chirakid kaweesirikon <chirakid.k@gmail.com>
2023-08-27 23:11:38 +0700

    :art: fixed
    fixed

--- plugins/google-recaptcha.ts
+++ plugins/google-recaptcha.ts
@@ -1,7 +1,7 @@
 import { VueReCaptcha } from 'vue-recaptcha-v3';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  ;
+
   nuxtApp.vueApp.use(VueReCaptcha, {
     siteKey: useRuntimeConfig().public.recaptchaSiteKey,
     loaderOptions: {
